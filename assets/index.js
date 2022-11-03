aebdf3952d75d052ce45951a25763839c143434c
ziyadovamid <amid.ziyadov25@gmail.com>
2022-11-03 03:34:30 -0700

    amid

--- assets/index.js
+++ assets/index.js
@@ -1,54 +1,108 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+let myNodelist = document.getElementsByTagName("LI");
+let i;
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+const sortButton = document.querySelector('#myDIV>img')
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+let liElements = document.getElementById('myUL')
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+liElements.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
     }
-  }
+})
+
+
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Bir sheyler elave et!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+}
+
+
+
+sortButton.addEventListener("click", () => {
+
+
+    let ar = []
+    const data = [...liElements?.children]
+    data.forEach(item => {
+        ar.push(item.innerHTML)
+    })
+    ar.sort();
+    liElements.innerHTML = ""
+    ar.forEach(item => {
+        let li = `<li>${item}</li>`
+
+        liElements.innerHTML += li
+    })
+
+
+})
+
+
+
+
+
+let sekil1 = document.querySelector('.sekil2')
+
+sekil1.addEventListener("click", () => {
+
+    let ar = []
+    const data = [...liElements?.children]
+    data.forEach(item => {
+        ar.push(item.innerHTML)
+    })
+    ar.sort();
+    ar.reverse();
+    liElements.innerHTML = ""
+    ar.forEach(item => {
+        let li = `<li>${item}</li>`
+
+        liElements.innerHTML += li
+    })
+
+})
+
+
+
+sekil1.addEventListener("click", () => {
+    sortButton.style.display = "block";
+    sekil1.style.display = "none";
+    let arr = [];
+    list.push(arr);
+    arr.forEach((item) => {
+        item.sort();
+        item.reverse();
+    });
+})
+
+
+if (
+    sortButton.addEventListener("click", () => {
+        sekil1.style.display = "block";
+        sortButton.style.display = "none";
+    })
+) {
 }
